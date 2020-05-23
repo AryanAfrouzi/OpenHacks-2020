64b04a2663b640f03c6943075b937f59c237344b
Eric Xiao <42785168+mathlord2@users.noreply.github.com>
2020-05-23 18:07:57 -0400

    Added map

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,10 +1,12 @@
 <script>
 	import Upload from "./Upload.svelte";
+	import Map from "./Map.svelte";
 </script>
 
 <main>
 	<h1>Take a picture:</h1>
 	<Upload/>
+	<Map/>
 </main>
 
 <style>
---
+++ client/src/Map.svelte
@@ -0,0 +1,51 @@
+<script>
+    import {onMount} from 'svelte';
+    let map, heatmap;
+
+    onMount(() => {
+        map = new google.maps.Map(document.getElementById("map-canvas"), {
+          zoom: 13,
+          center: {lat: 37.775, lng: -122.434},
+          mapTypeId: 'satellite'
+        });
+
+        heatmap = new google.maps.visualization.HeatmapLayer({
+          data: getPoints(),
+          map: map
+        });
+    });
+
+    function getPoints() {
+        return [
+            {location: new google.maps.LatLng(37.782, -122.447), weight: 0.5},
+            new google.maps.LatLng(37.782, -122.445),
+            {location: new google.maps.LatLng(37.782, -122.443), weight: 2},
+            {location: new google.maps.LatLng(37.782, -122.441), weight: 3},
+            {location: new google.maps.LatLng(37.782, -122.439), weight: 2},
+            new google.maps.LatLng(37.782, -122.437),
+            {location: new google.maps.LatLng(37.782, -122.435), weight: 0.5},
+            {location: new google.maps.LatLng(37.785, -122.447), weight: 3},
+            {location: new google.maps.LatLng(37.785, -122.445), weight: 2},
+            new google.maps.LatLng(37.785, -122.443),
+            {location: new google.maps.LatLng(37.785, -122.441), weight: 0.5},
+            new google.maps.LatLng(37.785, -122.439),
+            {location: new google.maps.LatLng(37.785, -122.437), weight: 2},
+            {location: new google.maps.LatLng(37.785, -122.435), weight: 3}
+        ];
+    }
+        
+</script>
+
+<div id="map">
+    <div id="map-canvas"></div>
+</div>
+
+<style>
+    #map-canvas {
+        margin: 30px 0px;
+        width: 60%;
+        height: 50%;
+        position: absolute;
+        margin-left: 20%;
+    }
+</style>
