05a1389b0e74c2b3b189044cdec2375545526e3a
tejas-srikanth <tejas.c.srikanth.2020@outlook.com>
2020-05-24 09:30:09 -0400

    commit1

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,6 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
+
 import pickle
 
 labelsPath="yolo_v3/coco.names"
@@ -24,3 +25,13 @@ def main():
     img = request.files["image"].read()
     img = img.open(io.BytesIO(img))
     npimg = np.array
+    image=npimg.copy()
+    image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    res=evaluate(image,nets,Lables,Colors)
+    image=cv2.cvtColor(res,cv2.COLOR_BGR2RGB)
+    np_img=Image.fromarray(image)
+    img_encoded=image_to_byte_array(np_img)
+    return Response(response=img_encoded, status=200,mimetype="image/jpeg"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
